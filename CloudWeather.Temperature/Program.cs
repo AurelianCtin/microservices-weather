20f4e5713ccea3c0c196bdd0540a54e7806a246d
Aurelian Angheloiu <aurelian.angheloiu@waters.com>
2023-07-31 14:55:34 +0300

    Add endpoints for services

--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,4 +1,5 @@
 using CloudWeather.Temperature.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,5 +11,26 @@ builder.Services.AddDbContext<TemperatureDbContext>(opts => {
 }, ServiceLifetime.Transient);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, TemperatureDbContext db) =>
+{
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query param between 1 and 30.");
+    }
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Temperature.Where(t => t.ZipCode == zip && t.CreatedOn > startDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
+});
+
+app.MapPost("/observation", async (Temperature temperature, TemperatureDbContext db) => {
+    temperature.CreatedOn = temperature.CreatedOn.ToUniversalTime();
+
+    await db.AddAsync(temperature);
+    await db.SaveChangesAsync();
+});
 
 app.Run();
